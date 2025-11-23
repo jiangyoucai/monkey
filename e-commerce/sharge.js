6fd164cffc77939b729da7a03681b81ec7bdfc4c
andy <andy@Mac-mini.local>
2025-11-23 09:40:40 +0800

    update new script

--- e-commerce/sharge.js
+++ e-commerce/sharge.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Snapsbot x Dockcase
+// @name         Snapsbot x Sharge
 // @namespace    https://snapsbot.com
-// @version      0.0.2
-// @description  多凯斯产品详情页图片下载
+// @version      0.0.1
+// @description  闪极产品详情页图片下载
 // @author       andy.jiang
-// @match        *://www.dockcase.com/products/*
+// @match        *://sharge.com/products/*
 // @require      https://cdn.jsdelivr.net/npm/jszip@3.10.1/dist/jszip.min.js
 // @require      https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -15,7 +15,7 @@
   "use strict";
 
   const code = {
-    menu: `<div style="position:fixed;width:56px;height:56px;right:0;bottom:50px;display:flex;flex-direction:column;justify-content:center;align-items:center;background-color:#fff;border-radius:18px 0 0 18px;cursor:pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6" style="width:20px;height:20px"><path fill-rule="evenodd" d="M5.478 5.559A1.5 1.5 0 0 1 6.912 4.5H9A.75.75 0 0 0 9 3H6.912a3 3 0 0 0-2.868 2.118l-2.411 7.838a3 3 0 0 0-.133.882V18a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0 0 17.088 3H15a.75.75 0 0 0 0 1.5h2.088a1.5 1.5 0 0 1 1.434 1.059l2.213 7.191H17.89a3 3 0 0 0-2.684 1.658l-.256.513a1.5 1.5 0 0 1-1.342.829h-3.218a1.5 1.5 0 0 1-1.342-.83l-.256-.512a3 3 0 0 0-2.684-1.658H3.265l2.213-7.191Z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v6.44l1.72-1.72a.75.75 0 1 1 1.06 1.06l-3 3a.75.75 0 0 1-1.06 0l-3-3a.75.75 0 0 1 1.06-1.06l1.72 1.72V3a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd"/></svg></div>`,
+    menu: `<div style="position:fixed;width:45px;height:45px;right:41px;bottom:142px;display:flex;flex-direction:column;justify-content:center;align-items:center;background-color:#fff;border-raduis:2px;cursor:pointer"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6" style="width:20px;height:20px"><path fill-rule="evenodd" d="M5.478 5.559A1.5 1.5 0 0 1 6.912 4.5H9A.75.75 0 0 0 9 3H6.912a3 3 0 0 0-2.868 2.118l-2.411 7.838a3 3 0 0 0-.133.882V18a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3v-4.162c0-.299-.045-.596-.133-.882l-2.412-7.838A3 3 0 0 0 17.088 3H15a.75.75 0 0 0 0 1.5h2.088a1.5 1.5 0 0 1 1.434 1.059l2.213 7.191H17.89a3 3 0 0 0-2.684 1.658l-.256.513a1.5 1.5 0 0 1-1.342.829h-3.218a1.5 1.5 0 0 1-1.342-.83l-.256-.512a3 3 0 0 0-2.684-1.658H3.265l2.213-7.191Z" clip-rule="evenodd"/><path fill-rule="evenodd" d="M12 2.25a.75.75 0 0 1 .75.75v6.44l1.72-1.72a.75.75 0 1 1 1.06 1.06l-3 3a.75.75 0 0 1-1.06 0l-3-3a.75.75 0 0 1 1.06-1.06l1.72 1.72V3a.75.75 0 0 1 .75-.75Z" clip-rule="evenodd"/></svg></div>`,
     progress: `<div style="position:fixed;top:0;left:0;z-index:100;background:rgba(0,0,0,.5);width:100vw;height:100vh;display:flex;justify-content:center;align-items:center"><div style="background:#fff;width:256px;height:160px;border-radius:16px;overflow:hidden;display:flex;justify-content:center;align-items:center;flex-direction:column"><div style="text-align:center"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" style="height:24px;margin:8px auto;color:gray"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/></svg></div><div style="font-size:14px">下载 <span id="current">-</span> <span style="margin:0 2px">/</span> <span id="total">-</span> 张</div></div></div>`,
   };
 
@@ -55,9 +55,7 @@
     console.log("-----------video-----------");
 
     console.log("-----------thumb-----------");
-    const thumbList = document.getElementsByClassName(
-      "product-gallery__thumbnail",
-    );
+    const thumbList = document.getElementsByClassName("product__thumbnail");
     for (let i = 0; i < thumbList.length; i++) {
       const item = thumbList[i];
       const img = item.getElementsByTagName("img");
@@ -75,27 +73,16 @@
     }
 
     console.log("-----------sku-----------");
-    const skuTag = document.getElementsByClassName("goodsDsc__color");
-    if (skuTag.length > 0) {
-      const skuList = skuTag[0].getElementsByTagName("img");
-      for (let i = 0; i < skuList.length; i++) {
-        const item = skuList[i];
-        const source = item.getAttribute("src");
-        if (source && isURL(source)) {
-          const path = getPath(source);
-          const name = getFile("sku", i, getExtension(path));
-
-          hub.push({ name: name, url: path });
-        }
-      }
-    }
 
     console.log("-----------detail-----------");
-    const richList = document.getElementsByClassName("section-four");
+    const richList = document.getElementsByClassName(
+      "station-tabs-default-tabpanel",
+    );
     for (let i = 0; i < richList.length; i++) {
       const tmp = richList[i].getElementsByTagName("img");
       for (let index = 0; index < tmp.length; index++) {
         const item = tmp[index];
+
         const source = item.getAttribute("src");
         if (source && isURL(source)) {
           const path = getPath(source);
@@ -111,6 +98,10 @@
   };
 
   const isURL = (source) => {
+    if (source.startsWith("//")) {
+      source = "https:" + source;
+    }
+
     try {
       return Boolean(new URL(source));
     } catch (e) {
